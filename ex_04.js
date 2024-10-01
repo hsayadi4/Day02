e4641f4f2aa9ad5dc51024b9cf775d7939c2ca81
hsayadi4 <hamza.sayadi@epitech.digital>
2024-10-01 17:47:34 +0200

    Create ex_04.js

---
+++ ex_04.js
@@ -0,0 +1,17 @@
+let a = 2;
+let b = 4
+let action = "sub";
+
+function calculator(){
+if (action === "add")
+return addition(a,b)
+if (action === "sub")
+return substraction(a,b)
+}
+
+function addition() {
+return a + b 
+
+}
+function substraction(){
+return a - b
